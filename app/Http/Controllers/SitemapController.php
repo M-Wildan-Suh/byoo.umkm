dcffe9a7f4559f79ec8444d2ed21c4dd61beb187
wildan <wildansuhendar87@gmail.com>
2025-01-13 14:17:37 +0700

    test

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -11,17 +11,17 @@ class SitemapController extends Controller
 {
     public function index()
     {
-        $sitemap = Sitemap::create()
-            ->add(Url::create('/')->setLastModificationDate(now()))
-            ->add(Url::create('/product')->setLastModificationDate(now()));
+        // $sitemap = Sitemap::create()
+        //     ->add(Url::create('/')->setLastModificationDate(now()))
+        //     ->add(Url::create('/product')->setLastModificationDate(now()));
 
-        // Dynamically add more URLs if needed, such as from a database
-        foreach (Product::all() as $model) {
-            $slug = Str::slug($model->name, '-');
-            $sitemap->add(Url::create("/{$slug}")->setLastModificationDate($model->updated_at));
-        }
+        // // Dynamically add more URLs if needed, such as from a database
+        // foreach (Product::all() as $model) {
+        //     $slug = Str::slug($model->name, '-');
+        //     $sitemap->add(Url::create("/{$slug}")->setLastModificationDate($model->updated_at));
+        // }
 
-        $sitemap->writeToFile(public_path('sitemap.xml'));
+        // $sitemap->writeToFile(public_path('sitemap.xml'));
         // return response()->download(public_path('sitemap.xml'));
     }
 }

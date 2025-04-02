e24a18c93e5ab17a26ec8dcd274789abd91d7a4e
wildan <wildansuhendar87@gmail.com>
2025-04-02 11:38:01 +0700

    forget

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -28,7 +28,7 @@
                                     src="{{ asset('storage/images/product/' . $item->image) }}" alt="">
                                 <div class=" absolute bottom-4 left-4 z-20 flex flex-wrap gap-2">
                                     @foreach ($item->category as $category)
-                                        <div class=" text-xs sm:text-sm backdrop-blur bg-black/50 cursor-default rounded-full border-2 border-[#ff7100] text-[#ff7100] px-2 py-1">Test</div>
+                                        <div class=" text-xs sm:text-sm backdrop-blur bg-black/50 cursor-default rounded-full border-2 border-[#ff7100] text-[#ff7100] px-2 py-1">{{$category->category}}</div>
                                     @endforeach
                                 </div>
                             </div>
